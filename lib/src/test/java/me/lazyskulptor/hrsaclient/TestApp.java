6742962469da07ba2368973fe0858f4b3a887511
박현준(Hyeonjun Park) <lazyskulptor@gmail.com>
2023-09-16 14:19:17 +0900

    Test Env is built (#5)
    * Test Env is built, Test script is added

---
+++ lib/src/test/java/me/lazyskulptor/hrsaclient/TestApp.java
@@ -0,0 +1,12 @@
+package me.lazyskulptor.hrsaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class TestApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApp.class, args);
+    }
+}
