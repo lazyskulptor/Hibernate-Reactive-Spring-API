83ba5ea104b2546cfc229d327d0d7c63e20dc7eb
Park Hyeonjun <lazyskulptor@gmail.com>
2023-12-23 17:10:31 +0900

    Transactional Test for Save and Find is done

--- test/src/main/java/me/lazyskulptor/demo/service/AccountService.java
+++ test/src/main/java/me/lazyskulptor/demo/service/AccountService.java
@@ -1,10 +1,14 @@
 package me.lazyskulptor.demo.service;
 
+import me.lazyskulptor.demo.model.Account;
 import me.lazyskulptor.demo.repo.AccountQueryRepository;
 import me.lazyskulptor.demo.spec.Logic;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Transactional
 @Service
@@ -17,5 +21,15 @@ public class AccountService {
 
     public Mono<Long> count() {
         return queryRepository.count(Logic.TRUE);
+    }
+
+    public Mono<List<Account>> findAll() {
+        return queryRepository.findAll(Sort.by("id"))
+                .collectList();
+    }
+
+    public Mono<Void> save(Account account) {
+        return queryRepository.saveAndFlush(account)
+                .then();
     }
 }
--- test/src/test/java/me/lazyskulptor/demo/repo/TxManagerTest.java
+++ test/src/test/java/me/lazyskulptor/demo/repo/TxManagerTest.java
@@ -1,15 +1,21 @@
 package me.lazyskulptor.demo.repo;
 
 import me.lazyskulptor.demo.ContainerExtension;
+import me.lazyskulptor.demo.model.Account;
+import me.lazyskulptor.demo.model.Authority;
 import me.lazyskulptor.demo.service.AccountService;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.ReactiveTransactionManager;
 import reactor.core.publisher.Mono;
+
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -23,7 +29,7 @@ public class TxManagerTest {
     @SpyBean
     ReactiveTransactionManager transactionManager;
 
-    @MockBean
+    @SpyBean
     AccountQueryRepository queryRepository;
     @Autowired
     AccountService service;
@@ -31,7 +37,7 @@ public class TxManagerTest {
     @Test
     void verifyCommitIsCalled() {
         final var value = 1L;
-        when(queryRepository.count(any())).thenReturn(Mono.just(value));
+        doReturn(Mono.just(value)).when(queryRepository).count(any());
         var result = service.count().block();
 
         verify(transactionManager, times(1)).commit(any());
@@ -41,9 +47,27 @@ public class TxManagerTest {
     }
 
     @Test
+    void verifySaveIsCommitted() {
+        Account account = Account.builder()
+                .username(RandomStringUtils.randomAlphanumeric(16))
+                .email(RandomStringUtils.randomAlphanumeric(8) + "@" + RandomStringUtils.randomAlphabetic(8) + ".com")
+                .password(RandomStringUtils.randomAlphanumeric(16))
+                .authorities(Set.of(Authority.MEMBER))
+                .enabled(true)
+                .build();
+
+        service.save(account).block();
+        var persisted = service.findAll().block();
+
+        assertThat(persisted).isNotEmpty();
+        verify(transactionManager, times(2)).commit(any());
+        verify(transactionManager, times(0)).rollback(any());
+    }
+
+    @Test
     void verifyRollbackIsCalled() {
         try {
-            when(queryRepository.count(any())).thenThrow(new RuntimeException("Test Exception"));
+            doThrow(new RuntimeException("Test Exception")).when(queryRepository).count(any());
 
             service.count().block();
         } catch (Exception e) {}
